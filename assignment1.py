081068a086de1510a5e01d25f0190f3590015b8d
manashee755 <malakarmanashee@gmail.com>
2025-04-30 19:58:16 +0530

    Add files via upload

---
+++ assignment1.py
@@ -0,0 +1,22 @@
+
+n1 = float(input("Enter the first number: "))
+n2 = float(input("Enter the second number: "))
+addition = n1 + n2
+subtraction = n1 - n2
+multiplication = n1 * n2
+division = n1 / n2
+print("Addition: ",addition)
+print(f"Subtraction: ",subtraction)
+print(f"Multiplication: ",multiplication)
+print(f"Division: ",division)
+
+
+
+#assignment=b
+
+
+n1=input('enter your first name: ')
+n2=input('enter your last name: ')
+
+c=n1+n2
+print('\nHello,'+c+'!Welcome to python program.')
